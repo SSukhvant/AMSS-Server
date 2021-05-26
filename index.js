5aa81c949a460c3a5f60a4fb19c1ad8d38c00d32
Sukhvant <sukhvantsingh581998@gmail.com>
2021-05-27 01:48:34 +0530

    Updated info

--- index.js
+++ index.js
@@ -12,32 +12,12 @@ app.use(express.json({ limit: '1mb' }));
 
 const database = new Datastore('database.db');
 database.loadDatabase();
-
-// app.get('/api', (request, response) => {
-//   database.find({}, (err, data) => {
-//     if (err) {
-//       response.end();
-//       return;
-//     }
-//     // const data1 = request.body;
-//     response.json(data);
-//   });
-// });
-
-// app.post('/api', (request, response) => {
-//   const data = request.body;
-//   const timestamp = Date.now();
-//   data.timestamp = timestamp;
-//   database.insert(data);
-//   response.json(data);
-// });
 app.get('/api', (request, response) => {
   database.find({}, (err, data) => {
     if (err) {
       response.end();
       return;
     }
-    // const data1 = request.body;
     response.json(data);
   });
 });
