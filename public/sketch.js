84f19977c5ddfd84f973e1a6907669aa344d7d6a
Sukhvant <sukhvantsingh581998@gmail.com>
2021-05-28 01:01:18 +0530

    Updated...

--- public/sketch.js
+++ public/sketch.js
@@ -1,15 +1,29 @@
-function setup() {
+async function setup() {
   let lat, lon;
+  async function getdata() {
+    const data = { lat, lon };
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  };
+  const response = await fetch('/hw', options);
+  const json = await response.json();
+  console.log(json);
+  }
+
   if ('geolocation' in navigator) {
     console.log('Geolocation available');
     navigator.geolocation.getCurrentPosition(position => {
       lat = position.coords.latitude;
       lon = position.coords.longitude;
       console.log(lat, lon);
-      document.getElementById('latitude').textContent = lat;
-      document.getElementById('longitude').textContent = lon;
+      getdata(lat,lon);
     });
   } else {
     console.log('Geolocation is not available');
   }
 }
+
